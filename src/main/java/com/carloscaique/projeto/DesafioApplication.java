52d0e342ab8f9a482e999269736d8e43b84d2046
Carlos Caique <caique-medeiros2011@hotmail.com>
2022-01-31 22:25:41 -0300

    Criando Operação de Instanciação

--- src/main/java/com/carloscaique/projeto/DesafioApplication.java
+++ src/main/java/com/carloscaique/projeto/DesafioApplication.java
@@ -1,13 +1,33 @@
 package com.carloscaique.projeto;
 
+import com.carloscaique.projeto.domain.Cliente;
+import com.carloscaique.projeto.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class DesafioApplication {
+public class DesafioApplication implements CommandLineRunner {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Cliente cli1 = new Cliente(null, "Carlos Caique" , "47125836978" , "932165498", "carlos@hotmail.com");
+		Cliente cli2 = new Cliente(null, "Caio Lima" , "14725836998" , "223696369", "caio@hotmail.com");
+		Cliente cli3 = new Cliente(null, "Joao Alves" , "74185236985" , "114741478", "joao@hotmail.com");
+
+		clienteRepository.saveAll(Arrays.asList(cli1,cli2,cli3));
+
+
+	}
 }
