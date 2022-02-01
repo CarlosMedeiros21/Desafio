1ebf9daff02326c03474974068326b07c84aa454
Carlos Caique <caique-medeiros2011@hotmail.com>
2022-01-31 21:39:58 -0300

    Testando Classe de Dominio Cliente

---
+++ src/main/java/com/carloscaique/projeto/domain/Cliente.java
@@ -0,0 +1,81 @@
+package com.carloscaique.projeto.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+// O cliente deve conter nome, cpf, telefone e email.
+
+public class Cliente implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+
+    private Cliente(){
+    }
+
+    public Cliente(Integer id, String nome, String cpf, String telefone, String email) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return id.equals(cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/carloscaique/projeto/resources/ClienteResource.java
+++ src/main/java/com/carloscaique/projeto/resources/ClienteResource.java
@@ -1,16 +1,30 @@
 package com.carloscaique.projeto.resources;
 
+import com.carloscaique.projeto.domain.Cliente;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController //  Retorna o objeto e os dados do objeto
 @RequestMapping(value = "/clientes") //
 public class ClienteResource {
 
     @RequestMapping(method = RequestMethod.GET) //
-    public String listar(){
-        return "Rest esta funcionando";
+    public List<Cliente> listar(){
+
+        Cliente cli1 = new Cliente(1, "Carlos Caique", "47125874126", "959874597", "caique@hotmail.com");
+        Cliente cli2 = new Cliente(2, "Caio Vinicius", "12345678965", "958745214", "caio@hotmail.com");
+        Cliente cli3 = new Cliente(3, "Jordane Medel", "98765432198", "921212525", "Jordane@hotmail.com");
+
+        List<Cliente> lista = new ArrayList<>();
+        lista.add(cli1);
+        lista.add(cli2);
+        lista.add(cli3);
+
+        return lista;
     }
 
 }
