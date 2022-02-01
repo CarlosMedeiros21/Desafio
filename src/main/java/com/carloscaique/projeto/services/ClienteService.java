d840259bb830d0af3d3cb56e183c25077713fd34
Carlos Caique <caique-medeiros2011@hotmail.com>
2022-01-31 22:42:38 -0300

    Ajustando Endpoint /Cliente/id

--- src/main/java/com/carloscaique/projeto/services/ClienteService.java
+++ src/main/java/com/carloscaique/projeto/services/ClienteService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import com.carloscaique.projeto.domain.Cliente;
 import com.carloscaique.projeto.repositories.ClienteRepository;
+import com.carloscaique.projeto.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ public class ClienteService {
 
     public Cliente find(Integer id) {
         Optional<Cliente> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
     }
 }
